b3014ad985fff9e3ce69f370e173ed1fce98d3e6
Martin Jönsson <martin.t.joensson@gmail.com>
2026-07-08 13:20:35 +0200

    Update: Broke out Card to its own component

---
+++ src/app/components/card.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Button from "./button";
+
+interface CardProps {
+  id?: number;
+  name: string;
+  gender?: string;
+  status?: string;
+  species?: string;
+  createdAt?: string;
+  image: string | null;
+}
+
+export default function Card({name, image}: CardProps) {
+    return (
+        <>
+        <h3 className="font-rubik font-bold"> {name}</h3>
+        <Button
+        text="Läs mer"
+        linkString="#"
+        className="bg-teal-500/50 border border-teal-200 hover:bg-teal-500/70 font-bold px-4 py-2 rounded-xl"
+        />
+        
+        <Image
+        className="order-first aspect-square object-cover w-full rounded-lg rounded-br-[125px] object-top"
+        src={
+            image ? image : "https://placehold.net/400x400.png"
+        }
+        alt={name}
+        width={400}
+        height={400}
+        unoptimized={image ? true : false}
+        />
+        </>
+    )
+}
--- src/app/grid.tsx
+++ src/app/grid.tsx
@@ -1,6 +1,7 @@
 import data from "@/app/data/characters.json";
 import Image from "next/image";
 import Button from "./components/button";
+import Card from "./components/card";
 
 export default function Grid() {
   const { page, pages, items: characters } = data;
@@ -16,24 +17,8 @@ export default function Grid() {
           <li
             key={char.id}
             className=" bg-zinc-800 grid *:not-[img]:m-4 border rounded-lg border-amber-500 "
-          >
-            <h3 className="font-rubik font-bold"> {char.name}</h3>
-            <Button
-              text="Läs mer"
-              linkString="#"
-              className="bg-teal-500/50 border border-teal-200 hover:bg-teal-500/70 font-bold px-4 py-2 rounded-xl"
-            />
-
-            <Image
-              className="order-first aspect-square object-cover w-full rounded-lg rounded-br-[125px] object-top"
-              src={
-                char.image ? char.image : "https://placehold.net/400x400.png"
-              }
-              alt={char.name}
-              width={400}
-              height={400}
-              unoptimized={char.image ? true : false}
-            />
+            >
+                <Card name={char.name} image={char.image} />
           </li>
         ))}
       </ul>
